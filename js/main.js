0a1ac66d171bf1c991574cf8cb705aa211e27a20
shmal-chel <shcmalivan@gmail.com>
2026-03-25 19:00:39 +0300

    modules in JS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-console.log("\n=== Деструктуризация массивов ===");
-const colors = ["red", "green", "blue"];
+import {greet, add, PI} from "./utils.js";
 
-const color1 = colors[0];
-const color2 = colors[1];
-console.log(color1, color2);
+// console.log("\n=== Деструктуризация массивов ===");
+// const colors = ["red", "green", "blue"];
 
-const [firstColor, secondColor, thirdColor] = colors;
-console.log(firstColor, secondColor, thirdColor);
+// const color1 = colors[0];
+// const color2 = colors[1];
+// console.log(color1, color2);
 
-const [primary, , tertiary] = colors;
-console.log(primary, tertiary)
+// const [firstColor, secondColor, thirdColor] = colors;
+// console.log(firstColor, secondColor, thirdColor);
 
-const [c1, c2, c3, c4 = "yellow"] = colors;
-console.log(c4);
+// const [primary, , tertiary] = colors;
+// console.log(primary, tertiary)
+
+// const [c1, c2, c3, c4 = "yellow"] = colors;
+// console.log(c4);
 
 // console.log("Декструризация объектов");
 
@@ -55,21 +57,86 @@ console.log(c4);
 // console.log()
 // printUser(user);
 
-const product = {
-    name: "Milk",
-    price: 120,
-    category: "Food",
-    inStock: true,
-};
+// const product = {
+//     name: "Milk",
+//     price: 120,
+//     category: "Food",
+//     inStock: true,
+// };
 
-const { name, price, category, inStock} = product;
-console.log(name, price, category, inStock);
+// const { name, price, category, inStock} = product;
+// console.log(name, price, category, inStock);
 
-function printProduct({name, price, category, inStock}){
-    console.log(`Name: ${name}`);
-    console.log(`Price: ${price}`);
-    console.log(`Category: ${category}`);
-    console.log(`InStock: ${inStock}`);
-}
+// function printProduct({name, price, category, inStock}){
+//     console.log(`Name: ${name}`);
+//     console.log(`Price: ${price}`);
+//     console.log(`Category: ${category}`);
+//     console.log(`InStock: ${inStock}`);
+// }
 
-printProduct(product);
+// printProduct(product);
+
+// console.log("Spread для массивов");
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+// const combined = [...arr1, ...arr2];
+// console.log("Объединенный массив:", combined);
+
+// const copy = [...arr1];
+// console.log("Еопия массива:", copy);
+
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив:", extended);
+
+// console.log("Spread для объектов");
+
+// const person = {
+//     name: "Диван",
+//     age: 14,
+// };
+
+// const address = {
+//     city: "Санкт-Петербруг",
+//     street: "Морской флотилии",
+// };
+
+// const fullInfo = {...person, ...address};
+// console.log("Полная информация:", fullInfo);
+
+// const personCopy = {...person};
+// console.log("копия объекта:", personCopy);
+
+// const updated = {...person, age: 16, occupation: "Developer"};
+// console.log("Обновленный объект:", updated);
+
+// console.log("Rest оператор");
+
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+
+// console.log("Сумма 1,2,3:", sum(1,2,3));
+// console.log("Сумма 1,2,3,4,5:", sum(1,2,3,4,5));
+
+// const numbers = [10,20,30,40,50];
+// const [first, second, ...rest] = numbers;
+// console.log("Первое число:", first);
+// console.log("Второе число:", second);
+// console.log("Остальные числа:", rest);
+
+// console.log("Модули");
+// console.log(greet("Алексей"));
+// console.log("5 + 3 =", add(5,3));
+// console.log("Значение PI:", PI);
+
+// import { multiply as умножить } from "./utils.js";
+// console.log("4 * 7 =", умножить(4, 7));
+
+// import * as Utils from "./utils.js";
+// console.log(Utils.greet("Мария"));
+// console.log("Умножение:", Utils.multiply(3, 9));
+
+import * as Math from "./math.js";
+console.log(Math.square(4));
+console.log(Math.cube(5));
+console.log(Math.E);
---
+++ js/math.js
@@ -0,0 +1,9 @@
+export function square(a) {
+    return a * a;
+}
+
+export function cube(a) {
+    return a * a * a;
+}
+
+export const E = 2.718;
